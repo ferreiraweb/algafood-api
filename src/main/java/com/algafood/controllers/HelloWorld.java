17523a70d3d176784c3722a2e8907b8e096af73e
ferreira <alexandreferreira.br@gmail.com>
2024-01-01 20:35:04 -0300

    first commit

---
+++ src/main/java/com/algafood/controllers/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.algafood.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> saudacoes() {
+        return ResponseEntity.ok("Hello Word");
+    }
+
+
+
+
+}
